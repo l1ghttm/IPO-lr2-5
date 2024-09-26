ae4881646ac00c9e98a9172ff94886ea02d6a15e
l1ghttm <ps4j@bk.ru>
2024-09-26 12:16:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import math
+import math 
 
-trapezoid_area(a, b, c, d):
+
    
-p = (a + b + c + d) / 2
 
 a = float(input("Введите длину основания a: "))
 b = float(input("Введите длину основания b: "))
 c = float(input("Введите длину боковой стороны c: "))
 d = float(input("Введите длину боковой стороны d: "))
 
-# Вычисление и вывод площади трапеции
-area = trapezoid_area(p*(p - a) * (p - b) * (p - c) * (p - d)) ** 0.5
+p = (a + b + c + d) / 2
+
+area = (p*(p - a) * (p - b) * (p - c) * (p - d)) ** 0.5
 print(f"Площадь трапеции: {area}")
